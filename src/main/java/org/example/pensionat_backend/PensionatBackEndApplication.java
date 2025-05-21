5d34e11f3bb6be24cf6be53d6a05078a7c08de08
AdamRitzler <adam.ritzler@gmail.com>
2025-05-21 17:10:52 +0200

    first upload

---
+++ src/main/java/org/example/pensionat_backend/PensionatBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.pensionat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionatBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PensionatBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pensionat_backend/PensionatBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pensionat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionatBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
