be2b7dc786f10a830961033adac947fda36438bc
Danial Ayazi <danne.esfahani@yahoo.com>
2025-05-24 13:11:35 +0200

    Added Customer entity with validation and bookings relationship

--- src/main/java/org/example/pensionat_backend/Models/Customer.java
+++ src/main/java/org/example/pensionat_backend/Models/Customer.java
@@ -1,20 +1,35 @@
 package org.example.pensionat_backend.Models;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+import java.util.List;
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Builder
 public class Customer {
+
         @Id
-        @GeneratedValue
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
+
+        @NotBlank(message = "Namn måste anges")
         private String name;
+
+        @Email(message = "Ogiltig emailadress")
+        @NotBlank(message = "Email måste anges")
+        @Column(unique = true)
         private String email;
+
+        @Pattern(regexp = "^\\d{7,15}$", message = "Telefonnummer måste bestå av 7 till 15 siffror")
         private String phone;
-    }
+
+        @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+        private List<Booking> bookings;
+}
