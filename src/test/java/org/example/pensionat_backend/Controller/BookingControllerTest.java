8959e33dc99fe891621260628d625c01ce848e10
AdamRitzler <adam.ritzler@gmail.com>
2025-10-02 20:08:34 +0200

    pls work

--- src/test/java/org/example/pensionat_backend/Controller/BookingControllerTest.java
+++ src/test/java/org/example/pensionat_backend/Controller/BookingControllerTest.java
@@ -13,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 class BookingControllerTest {
 
     private MockMvc mockMvc;
